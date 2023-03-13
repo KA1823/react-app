3a4d80568a014fcdaf35538904d67b352deb85c6
Kamran Anwar <kamran.anwar091@gmail.com>
2023-03-13 12:29:44 +0500

    minor-changes

--- src/component/todo/App.js
+++ src/component/todo/App.js
@@ -1,18 +1,18 @@
-// import logo from './logo.svg';
+import logo from "../../logo.svg";
 import '../style/App.css';
 import { useRef } from 'react';
 
 function App() {
 
   const refdata = useRef()
-const printdata = ()=>{
+const mydata = ()=>{
   console.log(refdata.current.value)
 }
   return (
     <div className="App">
       <header className="App-header">
         <input id="abc" ref={refdata} />
-        <button onClick={printdata}>submit</button>
+        <button onClick={mydata}>Click</button>
       </header>
     </div>
   );
