ccfefd68dbb37fb769795ecd8083ef6cd81a581d
Kamran Anwar <kamran.anwar091@gmail.com>
2023-03-22 11:04:03 +0500

    Usestate and useref array

--- src/component/todo/App.js
+++ src/component/todo/App.js
@@ -1,21 +1,81 @@
 import logo from "../../logo.svg";
 import '../style/App.css';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { Input, Card, Button } from "antd";
 
-function App() {
-
+function App() {     
+  
+  const [value, setValue] = useState([]);
   const refdata = useRef()
-const mydata = ()=>{
-  console.log(refdata.current.value)
+  
+  const addItem = () => {
+    setValue([
+      ...value, refdata.current.input.value])     
+  }
+
+  return(
+<>
+<Input ref={refdata}/>
+<Button onClick={()=>addItem()}>Button</Button>
+
+{value.map((item, index) => {
+return <Card key={index}>
+<p>{item}</p>
+</Card>
+})  
 }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <input id="abc" ref={refdata} />
-        <button onClick={mydata}>Click</button>
-      </header>
-    </div>
-  );
+</>
+);
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+//const refdata = useRef()
+//   const mydata = ()=>{
+//   console.log(refdata.current.value)
+// }
+//   return (
+//     <>
+    
+//       <header className="App-header">
+//         <input id="abc" ref={refdata} />
+//         <button onClick={mydata}>Click</button>
+//       </header>
+    
+//     </>
+//   );
+
+
+// const [value, setValue] = useState([]); 
+//   const [text, addText] = useState('')
+  
+//   const addItem = () => {
+//     setValue([
+//       ...value,
+//       text
+//     ])
+//     addText('')
+//   }
+//   // console.log(value, "results");
+//   return(
+// <>
+// <Input type="primary" value={text} onChange={(e) => {addText(e.target.value)}}/>
+// <Button onClick={addItem}>Button</Button>
+// {value.map((item, index) => {
+// return <Card key={index}>
+// <p>{item}</p>
+// </Card>
+// })
+// }
+// </>
+// );
